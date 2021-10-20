b26fe1b060c80cdff05d8ae5ea34d19c8fbaa988
Varnith <89956161+Varnithvarma@users.noreply.github.com>
2021-10-20 23:10:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your first Number"))
+b = int(input("Enter you second number "))
+ans = input("What you want to do Add,Sub,Mul,Div")
+if ans == "Add":
+    print("The answer of", a, "+", b, "is equal to", a + b)
+elif ans == "Sub"
+    print ("The answer of", a, "-", b, "is equal to", a - b)
+elif ans == "Mul":
+    print ("The answer of", a, "x", b, "is equal to", a * b)
+elif ans == "Div":
+    print ("The answer of", a, "÷", b, "is equal to", a / b):
+else:
+    print ("Error try again")
+    exit()
